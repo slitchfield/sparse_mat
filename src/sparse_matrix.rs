48c140fdc22cc47e9c5b1498094d0e688ded3e6d
Samuel Litchfield <samuel.litchfield@gmail.com>
2025-02-23 15:23:57 -0500

    First cut impl for row iteration, display.

--- src/sparse_matrix.rs
+++ src/sparse_matrix.rs
@@ -6,14 +6,99 @@ use std::collections::HashMap;
 struct SparseMatrix {
     shape: (u64, u64),
     values: HashMap<(u64, u64), f64>,
+
+    compressed_updated: bool,
+    compressed_rowarray: Vec<u64>,
+    compressed_colarray: Vec<u64>,
+    compressed_dataarray: Vec<f64>,
+
+    #[allow(dead_code)]
+    row_iter_idx: usize,
+}
+
+struct RowIterator<'a> {
+    matrix: &'a SparseMatrix,
+    row_iter_idx: usize,
+}
+
+impl Iterator for RowIterator<'_> {
+    // Iterate by rows
+    type Item = Vec<f64>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // Check for compressed updates here?
+        if self.row_iter_idx < self.matrix.shape.0 as usize {
+            let start = self.matrix.compressed_rowarray[self.row_iter_idx] as usize;
+            let end = self.matrix.compressed_rowarray[self.row_iter_idx + 1] as usize;
+
+            let colslice = &self.matrix.compressed_colarray[start..end];
+            let dataslice = &self.matrix.compressed_dataarray[start..end];
+
+            let mut retvec: Vec<f64> = vec![0.0; self.matrix.shape.1 as usize];
+
+            for (col, val) in std::iter::zip(colslice, dataslice) {
+                retvec[*col as usize] = *val;
+            }
+
+            self.row_iter_idx += 1;
+
+            Some(retvec)
+        } else {
+            None
+        }
+    }
 }
 
 impl SparseMatrix {
+    fn _update_compressed(&mut self) {
+        self.compressed_rowarray.clear();
+        self.compressed_colarray.clear();
+        self.compressed_dataarray.clear();
+
+        // Create row vecs that we'll sort by col
+        let mut row_vecs: Vec<Vec<(u64, f64)>> = vec![];
+        for _ in 0..self.shape.0 {
+            row_vecs.push(vec![]);
+        }
+
+        for ((row, col), val) in self.values.iter() {
+            row_vecs[*row as usize].push((*col, *val));
+        }
+        for rowidx in 0..self.shape.0 {
+            row_vecs[rowidx as usize].sort_by(|a, b| a.0.cmp(&b.0));
+        }
+
+        self.compressed_rowarray.push(0);
+        for row in row_vecs {
+            for (col, val) in row {
+                self.compressed_colarray.push(col);
+                self.compressed_dataarray.push(val);
+            }
+            self.compressed_rowarray
+                .push(self.compressed_dataarray.len() as u64);
+        }
+
+        self.compressed_updated = true
+    }
+
+    #[allow(dead_code)]
+    fn iter(&self) -> RowIterator {
+        RowIterator {
+            matrix: self,
+            row_iter_idx: 0,
+        }
+    }
+
     #[allow(dead_code)]
     fn new() -> SparseMatrix {
         SparseMatrix {
             shape: (0, 0),
             values: HashMap::new(),
+            compressed_updated: false,
+            compressed_rowarray: vec![],
+            compressed_colarray: vec![],
+            compressed_dataarray: vec![],
+            row_iter_idx: 0,
         }
     }
 
@@ -24,6 +109,11 @@ impl SparseMatrix {
         SparseMatrix {
             shape: (n, m),
             values: value_map,
+            compressed_updated: false,
+            compressed_rowarray: vec![],
+            compressed_colarray: vec![],
+            compressed_dataarray: vec![],
+            row_iter_idx: 0,
         }
     }
 
@@ -43,6 +133,7 @@ impl SparseMatrix {
         assert!(col < self.shape.1);
 
         self.values.insert((row, col), value);
+        self.compressed_updated = false;
     }
 
     #[allow(dead_code)]
@@ -53,6 +144,7 @@ impl SparseMatrix {
 
             self.values.insert((*row, *col), *val);
         }
+        self.compressed_updated = false;
     }
 
     #[allow(dead_code)]
@@ -61,6 +153,7 @@ impl SparseMatrix {
         assert!(row < self.shape.0);
         assert!(col < self.shape.1);
 
+        self.compressed_updated = false;
         self.values.remove(&(row, col))
     }
 
@@ -87,6 +180,14 @@ impl SparseMatrix {
         for ((row, col), val) in triplets {
             self.values.insert((col, row), val);
         }
+        self.compressed_updated = false;
+    }
+}
+
+use std::fmt;
+impl fmt::Display for SparseMatrix {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "shape: ({}, {})", self.shape.0, self.shape.1)
     }
 }
 
@@ -107,6 +208,27 @@ mod tests {
         assert!(local.peek_at(0, 0) == Some(1.0));
         assert!(local.peek_at(1, 1) == Some(1.0));
         assert!(local.peek_at(2, 2) == Some(1.0));
+    }
+
+    #[test]
+    fn sparsemat_rowiter() {
+        let mut local = sparse_matrix::SparseMatrix::empty_with_shape(4, 6);
+        local.insert_triplets(vec![
+            (0, 0, 10.0),
+            (0, 1, 20.0),
+            (1, 1, 30.0),
+            (2, 2, 50.0),
+            (1, 3, 40.0),
+            (2, 3, 60.0),
+            (2, 4, 70.0),
+            (3, 5, 80.0),
+        ]);
+
+        local._update_compressed();
+
+        for row in local.iter() {
+            dbg!(row);
+        }
     }
 
     #[test]
@@ -177,5 +299,50 @@ mod tests {
         assert!(local.peek_at(2, 2) == Some(3.0));
         assert!(local.peek_at(3, 2) == Some(4.0));
         assert!(local.peek_at(2, 3).is_none());
+    }
+
+    #[test]
+    fn sparsemat_compressedrepr() {
+        let mut local = sparse_matrix::SparseMatrix::empty_with_shape(4, 4);
+        local.insert_triplets(vec![(0, 0, 5.0), (1, 1, 8.0), (3, 1, 6.0), (2, 2, 3.0)]);
+        local._update_compressed();
+        assert!(local.compressed_dataarray == vec![5.0, 8.0, 3.0, 6.0]);
+        assert!(local.compressed_colarray == vec![0, 1, 2, 1]);
+        assert!(local.compressed_rowarray == vec![0, 1, 2, 3, 4]);
+
+        let mut local2 = sparse_matrix::SparseMatrix::empty_with_shape(4, 6);
+        local2.insert_triplets(vec![
+            (0, 0, 10.0),
+            (0, 1, 20.0),
+            (1, 1, 30.0),
+            (2, 2, 50.0),
+            (1, 3, 40.0),
+            (2, 3, 60.0),
+            (2, 4, 70.0),
+            (3, 5, 80.0),
+        ]);
+        local2._update_compressed();
+        assert!(
+            local2.compressed_dataarray == vec![10.0, 20.0, 30.0, 40.0, 50.0, 60.0, 70.0, 80.0]
+        );
+        assert!(local2.compressed_colarray == vec![0, 1, 1, 3, 2, 3, 4, 5]);
+        assert!(local2.compressed_rowarray == vec![0, 2, 4, 7, 8]);
+    }
+
+    #[test]
+    fn sparsemat_display() {
+        let mut local = sparse_matrix::SparseMatrix::empty_with_shape(4, 6);
+        local.insert_triplets(vec![
+            (0, 0, 10.0),
+            (0, 1, 20.0),
+            (1, 1, 30.0),
+            (2, 2, 50.0),
+            (1, 3, 40.0),
+            (2, 3, 60.0),
+            (2, 4, 70.0),
+            (3, 5, 80.0),
+        ]);
+
+        println!("{}", local);
     }
 }
