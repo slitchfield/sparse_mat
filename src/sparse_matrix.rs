821e29db5cc36d26b316cf70222b0e4396d1c3db
Samuel Litchfield <samuel.litchfield@gmail.com>
2025-02-23 13:21:58 -0500

    Impl identity, transpose, nnz.

--- src/sparse_matrix.rs
+++ src/sparse_matrix.rs
@@ -28,6 +28,15 @@ impl SparseMatrix {
     }
 
     #[allow(dead_code)]
+    fn identity(n: u64) -> SparseMatrix {
+        let mut local = SparseMatrix::empty_with_shape(n, n);
+        for diag_idx in 0..n {
+            local.insert(diag_idx, diag_idx, 1.0);
+        }
+        local
+    }
+
+    #[allow(dead_code)]
     fn insert(&mut self, row: u64, col: u64, value: f64) {
         // TODO: return result with oob error instead
         assert!(row < self.shape.0);
@@ -62,6 +71,23 @@ impl SparseMatrix {
 
         self.values.get(&(row, col)).copied()
     }
+
+    #[allow(dead_code)]
+    fn num_nonzero(&self) -> u64 {
+        self.values.len() as u64
+    }
+
+    #[allow(dead_code)]
+    fn transpose(&mut self) {
+        // Naive impl, could do better
+        self.shape = (self.shape.1, self.shape.0);
+
+        let triplets: Vec<((u64, u64), f64)> = self.values.drain().collect();
+
+        for ((row, col), val) in triplets {
+            self.values.insert((col, row), val);
+        }
+    }
 }
 
 #[cfg(test)]
@@ -72,6 +98,15 @@ mod tests {
     fn sparsemat_creation() {
         let _local = sparse_matrix::SparseMatrix::new();
         let _local2 = sparse_matrix::SparseMatrix::empty_with_shape(3, 3);
+    }
+
+    #[test]
+    fn sparsemat_identity_creation() {
+        let local = sparse_matrix::SparseMatrix::identity(3);
+        assert!(local.num_nonzero() == 3);
+        assert!(local.peek_at(0, 0) == Some(1.0));
+        assert!(local.peek_at(1, 1) == Some(1.0));
+        assert!(local.peek_at(2, 2) == Some(1.0));
     }
 
     #[test]
@@ -127,5 +162,20 @@ mod tests {
 
         assert!(local.peek_at(0, 0) == Some(1.0));
         assert!(local.peek_at(0, 1).is_none());
+    }
+
+    #[test]
+    fn sparsemat_transpose() {
+        let mut local = sparse_matrix::SparseMatrix::empty_with_shape(4, 4);
+
+        local.insert_triplets(vec![(0, 0, 1.0), (1, 0, 2.0), (2, 2, 3.0), (2, 3, 4.0)]);
+        local.transpose();
+
+        assert!(local.peek_at(0, 0) == Some(1.0));
+        assert!(local.peek_at(0, 1) == Some(2.0));
+        assert!(local.peek_at(1, 0).is_none());
+        assert!(local.peek_at(2, 2) == Some(3.0));
+        assert!(local.peek_at(3, 2) == Some(4.0));
+        assert!(local.peek_at(2, 3).is_none());
     }
 }
